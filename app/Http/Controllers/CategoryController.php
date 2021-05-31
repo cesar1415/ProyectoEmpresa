e713b0ed6b682e09107794c5857e3e8cb7836f13
cesar1415 <camartinez3359@misena.edu.co>
2021-05-31 14:17:01 -0500

    Configurando  category product provider

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Category\UpdateRequest;
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::get();
+        return view('admin.category.index', compact('categories'));
+    }
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+    public function store(StoreRequest $request)
+    {
+        Category::create($request->all());
+        return redirect()->route('categories.index');
+    }
+    public function show(Category $category)
+    {
+        return view('admin.category.show', compact('category'));
+    }
+    public function edit(Category $category)
+    {
+        return view('admin.category.show', compact('category'));
+    }
+    public function update(UpdateRequest $request, Category $category)
+    {
+        $category->update($request->all());
+        return redirect()->route('categories.index');
+    }
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('categories.index');
+    }
+}
---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Provider;
+use Illuminate\Http\Request;
+use App\Http\Requests\Provider\StoreRequest;
+use App\Http\Requests\Provider\UpdateRequest;
+
+class ProviderController extends Controller
+{
+    public function index()
+    {
+        $providers = Provider::get();
+        return view('admin.provider.index', compact('providers'));
+    }
+    public function create()
+    {
+        return view('admin.provider.create');
+    }
+    public function store(StoreRequest $request)
+    {
+        Provider::create($request->all());
+        return redirect()->route('providers.index');
+    }
+    public function show(Provider $provider)
+    {
+        return view('admin.provider.show', compact('provider'));
+    }
+    public function edit(Provider $provider)
+    {
+        return view('admin.provider.show', compact('provider'));
+    }
+    public function update(UpdateRequest $request, Provider $provider)
+    {
+        $provider->update($request->all());
+        return redirect()->route('providers.index');
+    }
+    public function destroy(Provider $provider)
+    {
+        $provider->delete();
+        return redirect()->route('providers.index');
+    }
+}
