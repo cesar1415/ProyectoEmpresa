22145a9e1a8fa1770237206eb335011174981b3a
cesar1415 <camartinez3359@misena.edu.co>
2021-06-04 16:28:12 -0500

    Creacion de views en provider

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -29,7 +29,7 @@ class ProviderController extends Controller
     }
     public function edit(Provider $provider)
     {
-        return view('admin.provider.show', compact('provider'));
+        return view('admin.provider.edit', compact('provider'));
     }
     public function update(UpdateRequest $request, Provider $provider)
     {
