d28143a71ea821e56e7ec7825cec7c0e8f9bb3d1
HackVidales <camartinez3359@misena.edu.co>
2021-11-20 09:56:29 -0500

    errores

--- public/js/app.js
+++ public/js/app.js
@@ -37410,8 +37410,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\puntodeventacurso\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\puntodeventacurso\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\sena\ProyectoEmpresa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\sena\ProyectoEmpresa\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
