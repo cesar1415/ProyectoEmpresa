d48c4dbcdbf2e230edc85ac3c59f7746ab8bc2c0
Semi-dios <tipouno@misena.edu.co>
2022-04-27 23:52:28 -0500

    bug-prod-barry/comment_class

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Package Service Providers...
